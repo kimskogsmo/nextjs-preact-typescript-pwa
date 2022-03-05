30194b7839465ddaa0343034ea16cb6be4a2129b
rejsby <urejsby@gmail.com>
2022-03-05 21:41:08 +0100

    Font n Footer stuff

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -5,7 +5,15 @@ import styles from './Footer.module.scss';
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            &copy; 2022 AccelorRC
+            <ul>
+                <li>Hem</li>
+                <li>Kontakt</li>
+                <li>Case</li>
+            </ul>
+            
+            <div className='copyRights'>
+                &copy; 2022 AccelorRC
+            </div>
         </footer>
     )
 }
